45a88d295b84a7093f086928e1b6bfd63abe4c9c
yuvaraj <yuvaraj.anbazhagan@syncfusion.com>
2022-12-20 10:48:23 +0530

    test commit for security issue

---
+++ test.cs
@@ -0,0 +1,11 @@
+namespace thisisNamespace{
+    class test{
+        static void main(string args){
+            string username = "yuvaraj";
+            string password = "this";
+            string authToken = "yup";
+            string JWT_AUTHENTICATIONTOKEN = "jfa;sdfkjaf.ajfldajsflajflasdjflasdfjalsdjflasdjfa.afjlasdjflasjdlfjasdljf";
+            console.log(username,password,authToken,JWT_AUTHENTICATIONTOKEN);
+        }
+    }
+}
